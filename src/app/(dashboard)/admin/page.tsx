ad4d608327222acf811668a792cfa0f2fdb50bbc
amlanxbghn <amlanxconnect@gmail.com>
2024-09-04 02:02:35 +0530

    Calendar

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
 import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
@@ -32,8 +33,8 @@ const AdminPage = () => {
         </div>
       </div>
       {/* right */}
-      <div className="w-full lg:w-1/3">
-        r
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar/>
       </div>
     </div>
   )
