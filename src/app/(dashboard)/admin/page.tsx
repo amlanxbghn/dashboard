587e6fac1e0a6695b49f3da4fd0b85c9698f9228
amlanxbghn <amlanxconnect@gmail.com>
2024-09-03 23:50:24 +0530

    User Cards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,23 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
-    <div className="">Admin Page</div>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* left */}
+      <div className="w-full lg:w-2/3">
+        {/* user cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student"/>
+          <UserCard type="teacher"/>
+          <UserCard type="parent"/>
+          <UserCard type="staff"/>
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full lg:w-1/3">
+        r
+      </div>
+    </div>
   )
 }
 
