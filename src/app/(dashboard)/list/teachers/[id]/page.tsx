8d59e3bcbe1f273fc0493aaf746bf1b1f2989277
amlanxbghn <amlanxconnect@gmail.com>
2024-10-31 15:30:35 +0530

    Forms

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Announcements from "@/components/Announcements"
 import BigCalendar from "@/components/BigCalendar"
+import FormModal from "@/components/FormModal"
 import Performance from "@/components/Performance"
 import Image from "next/image"
 import Link from "next/link"
@@ -14,26 +15,46 @@ const SingleTeacherPage = () => {
                     {/* USER INFO CARD */}
                     <div className="bg-dashboardSky py-6 px-4 rounded-md flex-1 flex gap-4">
                         <div className="w-1/3">
-                            <Image src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover"/>
+                            <Image src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover" />
                         </div>
                         <div className="w-2/3 flex flex-col justify-between gap-4">
-                            <h1 className="text-xl font-semibold">Tulshi Buragohain</h1>
+                            <div className="flex items-center gap-4">
+                                <h1 className="text-xl font-semibold">Tulshi Buragohain</h1>
+                                <FormModal
+                                    table="teacher"
+                                    type="update"
+                                    data={{
+                                        id: 1,
+                                        username: "deanguerrero",
+                                        email: "deanguerrero@gmail.com",
+                                        password: "password",
+                                        firstName: "Dean",
+                                        lastName: "Guerrero",
+                                        phone: "+1 234 567 89",
+                                        address: "1234 Main St, Anytown, USA",
+                                        bloodType: "A+",
+                                        dateOfBirth: "2000-01-01",
+                                        sex: "male",
+                                        img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
+                                    }}
+                                />
+                            </div>
                             <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                             <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/blood.png" alt="" width={14} height={14}/>
+                                    <Image src="/blood.png" alt="" width={14} height={14} />
                                     <span>A+</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/date.png" alt="" width={14} height={14}/>
+                                    <Image src="/date.png" alt="" width={14} height={14} />
                                     <span>January 2025</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/mail.png" alt="" width={14} height={14}/>
+                                    <Image src="/mail.png" alt="" width={14} height={14} />
                                     <span>user@gmail.com</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/phone.png" alt="" width={14} height={14}/>
+                                    <Image src="/phone.png" alt="" width={14} height={14} />
                                     <span>+1 234 567</span>
                                 </div>
                             </div>
@@ -43,11 +64,11 @@ const SingleTeacherPage = () => {
                     <div className="flex-1 flex gap-4 justify-between flex-wrap">
                         {/* CARD */}
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                            <Image 
-                                src="/singleAttendance.png" 
-                                alt="" 
-                                width={24} 
-                                height={24} 
+                            <Image
+                                src="/singleAttendance.png"
+                                alt=""
+                                width={24}
+                                height={24}
                                 className="w-6 h-6"
                             />
                             <div className="">
@@ -56,11 +77,11 @@ const SingleTeacherPage = () => {
                             </div>
                         </div>
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                            <Image 
-                                src="/singleBranch.png" 
-                                alt="" 
-                                width={24} 
-                                height={24} 
+                            <Image
+                                src="/singleBranch.png"
+                                alt=""
+                                width={24}
+                                height={24}
                                 className="w-6 h-6"
                             />
                             <div className="">
@@ -69,11 +90,11 @@ const SingleTeacherPage = () => {
                             </div>
                         </div>
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                            <Image 
-                                src="/singleLesson.png" 
-                                alt="" 
-                                width={24} 
-                                height={24} 
+                            <Image
+                                src="/singleLesson.png"
+                                alt=""
+                                width={24}
+                                height={24}
                                 className="w-6 h-6"
                             />
                             <div className="">
@@ -82,11 +103,11 @@ const SingleTeacherPage = () => {
                             </div>
                         </div>
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                            <Image 
-                                src="/singleClass.png" 
-                                alt="" 
-                                width={24} 
-                                height={24} 
+                            <Image
+                                src="/singleClass.png"
+                                alt=""
+                                width={24}
+                                height={24}
                                 className="w-6 h-6"
                             />
                             <div className="">
@@ -100,7 +121,7 @@ const SingleTeacherPage = () => {
                 {/* BOTTOM */}
                 <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
                     <h1>Teacher&apos;s Schedule</h1>
-                    <BigCalendar/>
+                    <BigCalendar />
                 </div>
             </div>
             {/* RIGHT */}
--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,7 +1,44 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
+// import TeacherForm from "./forms/TeacherForm";
+// import StudentForm from "./forms/StudentForm ";
+
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+    loading: () => <h1>Loading...</h1>,
+})
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+    loading: () => <h1>Loading...</h1>,
+})
+// const ParentForm = dynamic(() => import("./forms/ParentForm"));
+// const ClassForm = dynamic(() => import("./forms/ClassForm"));
+// const SubjectForm = dynamic(() => import("./forms/SubjectForm"));
+// const LessonForm = dynamic(() => import("./forms/LessonForm"));
+// const ExamForm = dynamic(() => import("./forms/ExamForm"));
+// const AssignmentForm = dynamic(() => import("./forms/AssignmentForm"));
+// const ResultForm = dynamic(() => import("./forms/ResultForm"));
+// const AttendanceForm = dynamic(() => import("./forms/AttendanceForm"));
+// const EventForm = dynamic(() => import("./forms/EventForm"));
+// const AnnouncementForm = dynamic(() => import("./forms/AnnouncementForm"));
+
+const forms: {
+    [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+    teacher: (type, data) => <TeacherForm type={type} data={data} />,
+    student: (type, data) => <StudentForm type={type} data={data} />,
+    // parent: (type, data) => <ParentForm type={type} data={data} />,
+    // subject: (type, data) => <SubjectForm type={type} data={data} />,
+    // class: (type, data) => <ClassForm type={type} data={data} />,
+    // lesson: (type, data) => <LessonForm type={type} data={data} />,
+    // exam: (type, data) => <ExamForm type={type} data={data} />,
+    // assignment: (type, data) => <AssignmentForm type={type} data={data} />,
+    // result: (type, data) => <ResultForm type={type} data={data} />,
+    // attendance: (type, data) => <AttendanceForm type={type} data={data} />,
+    // event: (type, data) => <EventForm type={type} data={data} />,
+    // announcement: (type, data) => <AnnouncementForm type={type} data={data} />,
+};
 
 const FormModal = ({
     table,
@@ -38,9 +75,9 @@ const FormModal = ({
                 <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}?</span>
                 <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
             </form>
-        ) : (
-            "create or update form"
-        );
+        ) : type === "create" || type === "update" ? (
+            forms[table](type, data)
+        ) : "Form not found!";
     };
 
     return (
